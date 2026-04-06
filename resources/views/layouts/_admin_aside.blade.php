ce054f70e76da0ddd158ebffb0dd1a64e38c9179
ibrahim albimani <abukhalil2b@gmail.com>
2026-04-06 20:00:03 +0400

    f

--- resources/views/layouts/_admin_aside.blade.php
+++ resources/views/layouts/_admin_aside.blade.php
@@ -6,13 +6,14 @@
     <a class="block my-1 p-2 rounded bg-white text-gray-900" href="{{ route('attendance.cate.index') }}">صفحة الإسئلة</a>
     @endhasPermission
 
+    @hasPermission('attendance.roundplay.create')
+    <a class="block my-1 p-2 rounded bg-white text-gray-900" href="{{ route('admin.attendance.roundplay.create') }}">الجولات</a>
+    @endhasPermission
+
     @hasPermission('attendance.user.create')
     <a class="block my-1 p-2 rounded bg-white text-gray-900" href="{{ route('admin.attendance.user.create') }}">المشاركين</a>
     @endhasPermission
 
-    @hasPermission('attendance.roundplay.create')
-    <a class="block my-1 p-2 rounded bg-white text-gray-900" href="{{ route('admin.attendance.roundplay.create') }}">الجولات</a>
-    @endhasPermission
 
     @hasPermission('attendance.cate.create')
     <a class="block my-1 p-2 rounded bg-white text-gray-900" href="{{ route('admin.attendance.cate.create') }}">الأسئلة</a>
